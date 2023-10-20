7cbec3ae1c0e6b2438a398b89071155bdd03ac75
DrinkTaiTea <67998203+DrinkTaiTea@users.noreply.github.com>
2023-10-20 19:32:24 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,13 +1,33 @@
-# streamlit_app.py
-
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+import streamlit_survey as ss
+import pandas as pd
+import numpy as np
 
-# Create a connection object.
-conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+def generate_survey():
+    survey = ss.StreamlitSurvey()
+    for i in range(1, 11):
+        question = f"Question {i}"
+        options = list(range(1, 11))
+        survey.radio(question, options)
+    return survey
 
-df = conn.read()
+def generate_responses():
+    responses = []
+    for i in range(30):
+        response = np.random.randint(1, 11, size=10)
+        responses.append(response)
+    return pd.DataFrame(responses)
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+def main():
+    st.set_page_config(page_title="Survey", page_icon=":pencil2:")
+    st.title("Survey")
+    st.write("Please answer the following questions:")
+    survey = generate_survey()
+    if survey.submit_button():
+        responses = generate_responses()
+        st.write("Thank you for your response!")
+        st.write("Here are the results:")
+        st.dataframe(responses)
+
+if __name__ == "__main__":
+    main()
